9a71ecf806f9248e7c21331fc9fc97cf7989fd28
PRO-GUNE <chathuragunasekera@gmail.com>
2023-07-14 20:36:20 +0530

    added elephant shooter

---
+++ src/elephantShooterController.cpp
@@ -0,0 +1,23 @@
+#include <Arduino.h>
+
+// Shooter pinouts
+
+// put function declarations here:
+int myFunction(int, int);
+
+void setup()
+{
+    // put your setup code here, to run once:
+    int result = myFunction(2, 3);
+}
+
+void loop()
+{
+    // put your main code here, to run repeatedly:
+}
+
+// put function definitions here:
+int myFunction(int x, int y)
+{
+    return x + y;
+}
